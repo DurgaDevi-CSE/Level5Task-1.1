49df0bc6067ff1c8705753018bd156954e6fab54
DurgaDevi-CSE <90144024+DurgaDevi-CSE@users.noreply.github.com>
2021-11-23 14:06:15 +0530

    Update todo.rb

--- Level5Task5.1/app/models/todo.rb
+++ Level5Task5.1/app/models/todo.rb
@@ -1,50 +1,56 @@
+require "active_record"
+require "./connect_db.rb"
+
 class Todo < ActiveRecord::Base
-  def self.today_day
-    where("ddate = ?", Date.today)
-    where(ddate: Date.today)
+  def due_today?
+    due_date == Date.today
   end
-  def today_day?
-    ddate == Date.today
+  def overdue?
+    due_date<Date.today
   end
-  def self.overduedate
-    where("ddate < ?", Date.today)
-  end
-
-  def self.ldate
-    where("ddate > ?", Date.today)
-  end
-  def self.add_task(h)
-    Todo.create!(todo_text: h[:todo_text], ddate: Date.today + h[:due_in_days], completed: false)
+  def due_later?
+    due_date>Date.today
+  end  
+  def self.overdue
+    where(due_date:Date.today)
   end
 
-  def self.mark_as_complete!(todo_id)
-    todolist= Todo.find(todo_id)
-    todolist.completed = true
-    todolist.save
-    todolist
+  def self.due_today
+    where(due_date: Date.today)
+  end
+
+  def self.due_later
+    where(due_date: Date.today)
   end
 
   def self.show_list
     puts "My Todo-list\n\n"
     puts "Overdue\n"
-    puts overduedate.map { |x| x.to_displayable_string }
+    puts overdue.map { |todolist| todolist.to_displayable_string }
     puts "\n\n"
     puts "Due Today\n"
-    puts today_day.map { |x| x.to_displayable_string }
+    puts due_today.map { |todolist| todolist.to_displayable_string }
     puts "\n\n"
     puts "Due Later\n"
-    puts ldate.map { |x| x.to_displayable_string }
+    puts due_later.map { |todolist| todolist.to_displayable_string }
     puts "\n\n"
   end
+
+  def self.add_task(h)
+    Todo.create!(todo_text: h[:todo_text], due_date: Date.today + h[:due_in_days], completed: false)
+  end
+
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
-    display_date = today_day? ? nil : ddate
+    display_date = due_today? ? nil : due_date
     "#{id}. #{display_status} #{todo_text} #{display_date}"
-  def self.add_task(x)
-    todo_text = x[:todo_text]
-    ddate = Date.today + todo[:due_in_days]
-    create!(todo_text: todo_text, ddate: ddate, completed: false)
+  def self.add_task(todo)
+    todo_text = todo[:todo_text]
+    due_date = Date.today + todo[:due_in_days]
+    create!(todo_text: todo_text, due_date: due_date, completed: false)
   end
+
+  
   def self.mark_as_complete(todo_id)
     todo_list = find(todo_id)
     todo_list.completed = true
